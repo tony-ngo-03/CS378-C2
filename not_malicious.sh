8f2d4a3e4ea9836fde08dde028bdff6bf8819105
tony-ngo-03 <tonyngo@utexas.edu>
2024-10-20 14:37:48 -0500

    trying shell script

--- not_malicious.sh
+++ not_malicious.sh
@@ -6,3 +6,18 @@ PASTEBIN_USER_NAME=""
 PASTEBIN_USER_PASS=""
 
 
+while true
+do
+    PASTE_LIST=$(curl -s -X POST \
+    -d "api_dev_key=$API_DEV_KEY" \
+    -d "api_user_key=$API_USER_KEY" \
+    -d "api_option=list" \
+    "https://pastebin.com/api/api_post.php")
+
+    echo $PASTE_LIST
+    sleep 1
+
+
+
+
+
